2cf701049dd0f02f87dd49b00cf14461dc9f07f4
Marcelino Chagas <marcelino.francisco.chagas@gmail.com>
2019-06-18 22:36:48 -0300

    Alteracoes

--- TuboSoft.c
+++ TuboSoft.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 typedef struct grafo{
-  int ponderado;    // 1-Ponderado 0-Nao e ponderado
-  int nro_vertices; // Numero de vertices do grafo
-  int grau_max;     // Numero MAX de arestas incidentes no Vertice
-  int **arestas;    // Matrix de Aresta que Liga os Grafos
-  float** pesos;    // Peso do Grafo
-  int* grau;        // Numero de arestas do grafo
-  struct pessoa *ps; // Referencia Struct de pessoas
+  int ponderado;        // 1-Ponderado 0-Nao e ponderado
+  int nro_vertices;     // Numero de vertices do grafo
+  int grau_max;         // Numero MAX de arestas incidentes no Vertice
+  int **arestas;        // Matrix de Aresta que Liga os Grafos
+  float** pesos;        // Peso do Grafo
+  int* grau;              // Numero de arestas do grafo
+  struct pessoa *ps;  // Referencia Struct de pessoas
 }Grafo;
 
 typedef struct pessoa{
@@ -18,6 +18,7 @@ typedef struct pessoa{
 }Pessoa;
 
 Grafo *cria_grafo(int nro_vertices, int grau_max, int ponderado){
+
   Grafo *gr = (Grafo *) malloc(sizeof(struct grafo));
 
   if(gr != NULL){
@@ -43,17 +44,77 @@ Grafo *cria_grafo(int nro_vertices, int grau_max, int ponderado){
             gr -> pesos = (float **)malloc(nro_vertices*sizeof(float*));
               for(i = 0; i < nro_vertices; i++)
                 gr -> pesos[i] = (float*)malloc(grau_max*sizeof(float));
-            }
+          }
     }
     return gr;
 }
 
-Pessoa *cidade(char nome, int populacao){
+int insereAresta(Grafo* gr, int orig, int dest, int eh_digrafo, float peso){
 
+    // Verifica se o Grafo e Nulo
+    if(gr == NULL)
+        return 0;
+
+    // Verifica se a origem do Vertice e maior que o nro de arestas
+    if(orig < 0 || orig >= gr->nro_vertices)
+        return 0;
+
+     // Verifica se o destino do Vertice e maior que o nro de arestas
+    if(dest < 0 || dest >= gr->nro_vertices)
+        return 0;
+
+    gr->arestas[orig][gr->grau[orig]] = dest;
+
+    if(gr-> ponderado)
+        gr->pesos[orig][gr->grau[orig]] = peso;
+
+    gr->grau[orig]++;
+
+    // Para nao duplicar caminho de Arestas
+    if(eh_digrafo == 0)
+        insereAresta(gr,dest,orig,1,peso);
+    return 1;
 }
-int main(void) {
-  int qtd_grafo[10][10];
 
-  printf("Hello World\n");
-  return 0;
+void libera_Grafo(Grafo* gr){
+    if(gr != NULL){
+        int i;
+        for(i = 0; i<gr -> nro_vertices; i++)
+            free(gr->arestas[i]);
+        free(gr->arestas);
+
+        if(gr->ponderado){
+            for(i = 0; i < gr->nro_vertices; i++)
+                free(gr->pesos[i]);
+            free(gr->pesos);
+        }
+        free(gr->grau);
+        free(gr);
+    }
+}
+
+
+void imprimir_Grafo(Grafo *gr){
+   for(int i = 0; gr->nro_vertices > i; i++){
+        for(int j = 0; gr->nro_vertices > j; j++){
+            printf("\t%i",gr->pesos[i][j]);
+        }
+        printf("\n");
+   }
+}
+
+
+int main(void) {
+    Grafo *gr;
+
+    gr = cria_grafo(5,2,1);
+
+    insereAresta(gr,1,2,0,10);
+    insereAresta(gr,2,3,0,20);
+    insereAresta(gr,4,5,0,30);
+
+    imprimir_Grafo(gr);
+
+
+    return 0;
 }
